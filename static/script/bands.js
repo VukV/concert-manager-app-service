b42ead48f74b6dce58ff0f3fe1d32258604acfa9
VukV <vuk.vukovic.2000@gmail.com>
2021-12-18 14:55:22 +0100

    added script files and fetches

--- static/script/bands.js
+++ static/script/bands.js
@@ -1,5 +1,46 @@
 window.addEventListener('load', init);
 
 function init(){
-    //TODO
+
+    //todo cookies, token
+
+    getBands();
+
+    document.getElementById("btn-add-band").addEventListener('click', e => {
+        e.preventDefault();
+
+        var data = {
+            name: document.getElementById('band-name').value,
+            country: document.getElementById('band-country').value,
+            year: document.getElementById('band-year').value,
+            genre: document.getElementById('band-genre').value
+        }
+
+        fetch('http://localhost:8081/admin/bands', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+                //TODO 'Authorization': 'Bearer ' + token
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(resElement => {
+                if(resElement.NESTO){
+                    //todo error
+                }
+                else{
+                    //todo dodaj bend u listu
+                }
+            });
+    });
+}
+
+function getBands(){
+    fetch('http://localhost:8081/admin/bands', {
+        headers: {
+            //TODO 'Authorization': 'Bearer ' + token
+        }
+    })
+    //response -> list item on page (html)
 }
--- static/script/venues.js
+++ static/script/venues.js
@@ -1,5 +1,46 @@
 window.addEventListener('load', init);
 
 function init(){
-    //TODO
+    
+    //todo cookies, token
+
+    getVenues();
+
+    document.getElementById("btn-add-venue").addEventListener('click', e => {
+        e.preventDefault();
+
+        var data = {
+            name: document.getElementById('venue-name').value,
+            capacity: document.getElementById('venue-capacity').value,
+            address: document.getElementById('venue-address').value,
+            website: document.getElementById('venue-website').value
+        }
+
+        fetch('http://localhost:8081/admin/venues', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+                //TODO 'Authorization': 'Bearer ' + token
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(resElement => {
+                if(resElement.NESTO){
+                    //todo error
+                }
+                else{
+                    //todo dodaj bend u listu
+                }
+            });
+    });
+}
+
+function getVenues(){
+    fetch('http://localhost:8081/admin/venues', {
+        headers: {
+            //TODO 'Authorization': 'Bearer ' + token
+        }
+    })
+    //response -> list item on page (html)
 }
